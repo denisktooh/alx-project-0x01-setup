10d17cb76ce61506ff71bb1fab1ec81f4ea0ac9b
denisktooh <199378976+denisktooh@users.noreply.github.com>
2025-11-11 18:39:23 +0300

    Fix checker requirements

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -22,7 +22,7 @@ const Users: React.FC<UsersPageProps> = ({ posts }) => {
             className="bg-blue-700 px-4 py-2 rounded-full text-white">Add User</button>
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-          {posts?.map((user: UserProps) => (
+          {posts.map((user: UserProps) => (
             <UserCard
               key={user.id}
               id={user.id}
